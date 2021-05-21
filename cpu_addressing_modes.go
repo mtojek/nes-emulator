ee597ebd8c70b6ba3ad7689cd41b31302e307705
Marcin Tojek <tojek.m@gmail.com>
2021-05-21 15:47:25 +0200

    More addressing modes

--- cpu_addressing_modes.go
+++ cpu_addressing_modes.go
@@ -44,7 +44,7 @@ func (c *cpu6502) abx() uint8 {
 
 	c.addrAbs = hi + lo + uint16(c.x)
 
-	if c.addrAbs&0x00FF != hi {
+	if c.addrAbs&0xFF00 != hi {
 		return 1
 	}
 	return 0
@@ -60,13 +60,54 @@ func (c *cpu6502) aby() uint8 {
 
 	c.addrAbs = hi + lo + uint16(c.y)
 
-	if c.addrAbs&0x00FF != hi {
+	if c.addrAbs&0xFF00 != hi {
 		return 1
 	}
 	return 0
 }
 
 // Indirect addressing
-func (c *cpu6502) in() uint8 {
+func (c *cpu6502) ind() uint8 {
+	ptrLo := uint16(c.read(c.pc))
+	c.pc++
+
+	ptrHi := uint16(c.read(c.pc)) << 8
+	c.pc++
+
+	ptr := ptrHi + ptrLo
+
+	if ptrLo == 0x00FF { // Simulate page boundary hardware bug
+		c.addrAbs = (uint16(c.read(ptr&0x00FF)) << 8) | uint16(c.read(ptr))
+		return 0
+	}
+
+	// Behave normally
+	c.addrAbs = (uint16(c.read(ptr+1)) << 8) | uint16(c.read(ptr))
+	return 0
+}
+
+// Indirect addressing of the zero-page with X offset
+func (c *cpu6502) izx() uint8 {
+	t := uint16(c.read(c.pc))
+	c.pc++
+
+	lo := uint16(c.read((t + uint16(c.x)) & 0x00FF))
+	hi := uint16(c.read((t+uint16(c.x)+1)&0x00FF)) << 8
+	c.addrAbs = hi + lo
+	return 0
+}
+
+// Indirect addressing of the zero-page with Y offset
+func (c *cpu6502) izy() uint8 {
+	t := uint16(c.read(c.pc))
+	c.pc++
+
+	lo := uint16(c.read(t & 0x00FF))
+	hi := uint16(c.read((t+1)&0x00FF)) << 8
+	c.addrAbs = hi + lo + uint16(c.y)
+
+	if c.addrAbs&0xFF00 != hi {
+		return 1
+	}
 	return 0
 }
