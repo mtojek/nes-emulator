a32d5d9545a60353e56b8846de074594cf138ac9
Marcin Tojek <tojek.m@gmail.com>
2021-05-23 16:32:02 +0200

    Comments

--- cartridge/ines.go
+++ cartridge/ines.go
@@ -4,9 +4,9 @@ type INESFileHeader struct {
 	Magic        uint32  // iNES magic number
 	PRGRomChunks uint8   // number of PRG-ROM banks (16KB each)
 	CHRRomChunks uint8   // number of CHR-ROM banks (8KB each)
-	Control1     uint8   // control bits
-	Control2     uint8   // control bits
-	PRGRamSize   uint8   // PRG-RAM size (x 8KB)
+	Control1     uint8   // control bits (mapper, mirroring, battery, trainer)
+	Control2     uint8   // control bits (mapper, VS/Playchoice, NES 2.0)
+	PRGRamSize   uint8   // PRG-RAM size (8KB each)
 	TVSystem1    uint8   // TV system
 	TVSystem2    uint8   // TV system
 	_            [5]byte // unused
