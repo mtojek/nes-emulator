9717c8c6e69428f6655fe8de28351cacc5312a55
mtojek <tojek.m@gmail.com>
2021-06-05 16:03:51 +0200

    Illegal opcode

--- cpu/cpu_opcodes.go
+++ cpu/cpu_opcodes.go
@@ -605,7 +605,7 @@ func (c *CPU6502) tya() uint8 {
 
 // Unknown instruction
 func (c *CPU6502) xxx() uint8 {
-	panic(fmt.Sprintf("unknown instruction: %02X, pc: %04x", c.opcode, c.pc))
+	fmt.Printf("unknown instruction: %02X, pc: %04x\n", c.opcode, c.pc)
 	return 0
 }
 
